a1f04a1b635dcaa6dae2dedc47333a44a84c6530
cr7vaibhav <vaibhavkandwaloo7@gmail.com>
2024-08-13 20:29:48 +0530

    playerTurn

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,13 +1,69 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) throws Exception {
-        char[][] board = { { '1', '2', '3' }, { '2', '3', '1' }, { '2', '1', '7' } };
+        char[][] board = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
 
         printBoard(board);
-        
-    }
-    
 
-    //method to print the board
+        Scanner sc = new Scanner(System.in);
+        playerTurn(board, sc);
+
+        printBoard(board);
+
+    }
+
+    private static void playerTurn(char[][] board, Scanner sc) {
+        System.out.println("Where would you like to play (1-9)");
+        String userInput = sc.nextLine();
+
+        // System.out.println(userInput);
+
+        // switch case to take userInput and put in the board array
+        switch (userInput) {
+            case "1":
+                board[0][0] = 'X';
+                break;
+
+            case "2":
+                board[0][1] = 'X';
+                break;
+
+            case "3":
+                board[0][2] = 'X';
+                break;
+
+            case "4":
+                board[1][0] = 'X';
+                break;
+
+            case "5":
+                board[1][1] = 'X';
+                break;
+
+            case "6":
+                board[1][2] = 'X';
+                break;
+
+            case "7":
+                board[2][0] = 'X';
+                break;
+
+            case "8":
+                board[2][1] = 'X';
+                break;
+
+            case "9":
+                board[2][2] = 'X';
+                break;
+
+            default:
+                System.out.println("D:");
+                break;
+        }
+    }
+
+    // method to print the board
     private static void printBoard(char[][] board) {
         System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
         System.out.println("-+-+-");
